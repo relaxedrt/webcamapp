f33a163c869f34b13c542e784cac882320e9d3bf
relaxedrt <rodrigotejeria01@gmail.com>
2023-02-23 16:42:38 +0100

    dasda

--- app.py
+++ app.py
@@ -8,7 +8,6 @@ cam = cv2.VideoCapture(0)
 #Guardamos el frame en una variable y si a leido bien o no en otra
 leido, frame = cam.read()
 
-
 #Evaluamos si se consiguio sacar la foto y la mostramos
 if leido == True:
     #Mostramos el resultado
