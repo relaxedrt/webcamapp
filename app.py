20d864802590562295eca2e3f24df460b23e916e
relaxedrt <rodrigotejeria01@gmail.com>
2023-02-23 16:36:59 +0100

    unimos la webcam y el pyplot

---
+++ app.py
@@ -0,0 +1,23 @@
+#IMPORTAMOS LAS LIBRERIAS NECESARIAS
+import cv2
+import matplotlib.pyplot as plt
+
+#Abrimos la camara
+cam = cv2.VideoCapture(0)
+
+#Guardamos el frame en una variable y si a leido bien o no en otra
+leido, frame = cam.read()
+
+
+#Evaluamos si se consiguio sacar la foto y la mostramos
+if leido == True:
+    #Mostramos el resultado
+    print("Foto tomada correctamente")
+    plt.imshow(frame)
+    plt.show()
+else:
+    print("Error al acceder a la camara")
+
+#Cerramos la webcams
+cam.release()
+
