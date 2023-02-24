cde638ae09a2213d80c820b4d300a2c87f8df837
relaxedrt <rodrigotejeria01@gmail.com>
2023-02-24 09:50:35 +0100

    Detectar azul en camara

--- app.py
+++ app.py
@@ -8,15 +8,23 @@ cam = cv2.VideoCapture(0)
 #Guardamos el frame en una variable y si a leido bien o no en otra
 leido, frame = cam.read()
 
+#ELEGIMOS EL UMBRAL DEL COLOR AZUL EN HSV
+umbral_bajo1 = (85,100,20)
+umbral_alto1 = (135,255,255)
+
 #Evaluamos si se consiguio sacar la foto y la mostramos
 if leido == True:
     #Mostramos el resultado
+    print("Foto tomada correctamente.")
+    print("Foto siendo procesada.")
     img = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-    print("Foto tomada correctamente")
-    plt.imshow(img)
+    mask1 = cv2.inRange(img, umbral_bajo1, umbral_alto1)
+    res = cv2.bitwise_and(img, img ,mask = mask1)
+    plt.imshow(res)
     plt.show()
+    print("Resultado mostrado.")
 else:
-    print("Error al acceder a la camara")
+    print("Error al acceder a la camara.")
 
 #Cerramos la webcams
 cam.release()
